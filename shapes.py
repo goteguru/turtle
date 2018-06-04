954f8ccf373059f729edd4d709f33062cfdb8c7e
gergogyerek <gergogyerek@gmail.com>
2018-06-04 20:05:19 +0200

    kezdeti koordinatak

--- shapes.py
+++ shapes.py
@@ -1,30 +1,30 @@
 from turtle import Turtle
 
 class Shapes:
-    def __init__(self):
+    def __init__(self, x=0, y=0):
+        self.turtle = Turtle(x,y)
         pass
 
 
 class Arrow(Shapes):
-    def __init__(self, direction, size):
+    def __init__(self, direction, size,x=0, y=0):
         self.direction = direction
         self.size = size
-        Shapes.__init__(self)
+        Shapes.__init__(self,x,y)
 
 class Rectangle(Shapes):
-    def __init__(self,height, width):
+    def __init__(self,height, width,x=0, y=0):
         self.height = height
         self.width = width
-        t = Turtle()
-        self.path = t.go(height).turn(90).go(width).turn(90).go(height).turn(90).go(width).path
-        Shapes.__init__(self)
+        Shapes.__init__(self,x,y)
+        self.path = self.turtle.go(height).turn(90).go(width).turn(90).go(height).turn(90).go(width).path
 
 class Polygon(Shapes):
-    def __init__(self,angle,size):
+    def __init__(self,angle,size,x=0, y=0):
         self.angle = angle
         self.size = size
-        t = Turtle()
         n = 360 / angle
-        self.path = n*(t.go(size).turn(angle)).path
-        Shapes.__init__(self)
+        Shapes.__init__(self,x,y)
+        self.path = n*(self.turtle.go(size).turn(angle)).path
+
 
